cef9d9d83247d82909bcbeb40bf7d203b9f7c2ba
Visa Putkinen <visa.putkinen@reaktor.fi>
2013-07-30 00:47:09 +0300

    Add interrupt handler for GC data line

--- gc_control.c
+++ gc_control.c
@@ -1,44 +1,55 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "usb_debug_only.h"
 #include "print.h"
 
-
-#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
- // Teensy 2.0: LED is active high
- #define LED_ON		(PORTD |= (1<<6))
- #define LED_OFF		(PORTD &= ~(1<<6))
-#else
- // Teensy 1.0: LED is active low
- #define LED_ON	(PORTD &= ~(1<<6))
- #define LED_OFF	(PORTD |= (1<<6))
-#endif
-
-#define LED_CONFIG	(DDRD |= (1<<6))
-#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
-
-int main(void)
+volatile uint16_t int0_count = 0;
+volatile uint8_t led_toggle_countdown = 100;
+ISR(INT0_vect)
 {
-	// set for 16 MHz clock, and make sure the LED is off
-	CPU_PRESCALE(0);
-	LED_CONFIG;
-	LED_OFF;
-
-	// initialize the USB, but don't want for the host to
-	// configure.
-	usb_init();
-
-  uint8_t led_on = 0;
-	// blink morse code messages!
-	while (1) {
-		print("Hello from Teensy++\n");
-    if (led_on)
-      LED_ON;
-    else
-      LED_OFF;
-    led_on = !led_on;
-		_delay_ms(1000);
-	}
+  ++int0_count;
+  --led_toggle_countdown;
 }
 
+#define LED_ON      (PORTD |= (1<<6))
+#define LED_OFF     (PORTD &= ~(1<<6))
+#define LED_TOGGLE  (PORTD ^= (1<<6))
+
+#define CPU_PRESCALE(n) (CLKPR=0x80, CLKPR=(n))
+
+//led=output, data=input w/o pull-up:
+#define CONFIG_DATA_AND_LED (DDRD=(1<<6), PORTD=(0))
+
+#define phex8 phex
+#define print_uintN_var(n, x) print_P(PSTR(#x "="));phex##n(x);pchar('\n');
+#define print_uint8_var(x) print_uintN_var(8, x);
+#define print_uint16_var(x) print_uintN_var(16, x);
+
+// PD0(INT0)=GC_DATA  PD6=LED
+int main(void)
+{
+  CPU_PRESCALE(0);
+  CONFIG_DATA_AND_LED;
+  LED_OFF;
+
+  usb_init();
+  //while(!usb_configured());
+
+  sei();
+  EICRA = 0x02; // Falling edge of GC_DATA(INT0) causes interrupt
+  EIMSK = 0x01; // Enable INT0
+
+  uint32_t print_interval = 200000;
+  while (1) {
+    if (led_toggle_countdown == 0) {
+      LED_TOGGLE;
+      led_toggle_countdown = 100;
+    }
+    if (--print_interval == 0) {
+      print_uint16_var(int0_count);
+      print_interval = 200000;
+    }
+  }
+}
