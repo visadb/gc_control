2e5c3e46a47e17fc658d3e24dfb5dc2e91775e2d
Visa Putkinen <visa.putkinen@reaktor.fi>
2013-08-14 16:49:58 +0300

    Make client wait for device if not present

--- client/gc_control_client.c
+++ client/gc_control_client.c
@@ -13,43 +13,54 @@
 
 
 static char get_keystroke(void);
+static void delay_ms(unsigned int msec);
 
+void use_device(void) {
+	int i, num;
+	char c, buf[64+1];
+
+  while (1) {
+    // check if any Raw HID packet has arrived
+    num = rawhid_recv(0, buf, 64, 10);
+    if (num < 0) {
+      printf("\nerror reading, device went offline\n");
+      //rawhid_close(0);
+      return;
+    }
+    if (num > 0) {
+      buf[64] = '\0';
+      printf("recv: %s", buf);
+    }
+    // check if any input on stdin
+    while ((c = get_keystroke()) >= 32) {
+      printf("\ngot key '%c', sending...\n", c);
+      buf[0] = c;
+      for (i=1; i<64; i++) {
+        buf[i] = 0;
+      }
+      rawhid_send(0, buf, 64, 100);
+    }
+  }
+}
 
 int main()
 {
-	int i, r, num;
-	char c, buf[64+1];
+	int r;
 
-	// C-based example is 16C0:0480:FFAB:0200
-	r = rawhid_open(1, 0x16C0, 0x0480, 0xFFEE, 0x0200);
-  if (r <= 0) {
-    printf("no rawhid device found\n");
-    return -1;
+	printf("Waiting for device:");
+	fflush(stdout);
+  while (1) {
+    r = rawhid_open(1, 0x16C0, 0x0480, 0xFFEE, 0x0200);
+    if (r <= 0) {
+      printf(".");
+			fflush(stdout);
+      delay_ms(1000);
+      continue;
+    }
+    printf("found rawhid device\n");
+
+    use_device();
   }
-	printf("found rawhid device\n");
-
-	while (1) {
-		// check if any Raw HID packet has arrived
-		num = rawhid_recv(0, buf, 64, 220);
-		if (num < 0) {
-			printf("\nerror reading, device went offline\n");
-			rawhid_close(0);
-			return 0;
-		}
-		if (num > 0) {
-      buf[64] = '\0';
-			printf("recv: %s", buf);
-		}
-		// check if any input on stdin
-		while ((c = get_keystroke()) >= 32) {
-			printf("\ngot key '%c', sending...\n", c);
-			buf[0] = c;
-			for (i=1; i<64; i++) {
-				buf[i] = 0;
-			}
-			rawhid_send(0, buf, 64, 100);
-		}
-	}
 }
 
 #if defined(OS_LINUX) || defined(OS_MACOSX)
@@ -90,3 +101,16 @@ static char get_keystroke(void)
 }
 
 
+#if (defined(WIN32) || defined(WINDOWS) || defined(__WINDOWS__))
+#include <windows.h>
+static void delay_ms(unsigned int msec)
+{
+	Sleep(msec);
+}
+#else
+#include <unistd.h>
+static void delay_ms(unsigned int msec)
+{
+	usleep(msec * 1000);
+}
+#endif
